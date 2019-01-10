88d417509fa471895d6936cc9b75ab42f917b2e4
mikefaraponov <mikefaraponov@gmail.com>
2019-01-10 22:52:22 +0200

    Refactored

--- encoding/info/info.go
+++ encoding/info/info.go
@@ -8,17 +8,6 @@ import (
 )
 
 func Unmarshal(data []byte, v interface{}) error {
-	info := make(map[string]string)
-
-	for _, line := range strings.Split(string(data), "\r\n") {
-		if len(line) != 0 && !strings.HasPrefix(line, "#") {
-			result := strings.Split(line, ":")
-			if len(result) == 2 {
-				info[result[0]] = result[1]
-			}
-		}
-	}
-
 	rv := reflect.ValueOf(v)
 
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
@@ -29,8 +18,14 @@ func Unmarshal(data []byte, v interface{}) error {
 		return errors.New("")
 	}
 
-	for i := 0; i < rv.Elem().NumField(); i++ {
-		field := rv.Elem().Type().Field(i)
+	info := parseInfo(string(data))
+
+	return fillObjectFields(info, rv)
+}
+
+func fillObjectFields(info map[string]string, v reflect.Value) error {
+	for i := 0; i < v.Elem().NumField(); i++ {
+		field := v.Elem().Type().Field(i)
 
 		tag := field.Tag.Get("info")
 
@@ -42,18 +37,32 @@ func Unmarshal(data []byte, v interface{}) error {
 			if err != nil {
 				return err
 			}
-			rv.Elem().Field(i).SetInt(result)
+			v.Elem().Field(i).SetInt(result)
 		case reflect.String:
-			rv.Elem().Field(i).SetString(value)
+			v.Elem().Field(i).SetString(value)
 		case reflect.Float64:
 			reflect.TypeOf(new(int))
 			result, err := strconv.ParseFloat(value, 64)
 			if err != nil {
 				return err
 			}
-			rv.Elem().Field(i).SetFloat(result)
+			v.Elem().Field(i).SetFloat(result)
+		}
+	}
+	return nil
+}
+
+func parseInfo(data string) map[string]string {
+	info := make(map[string]string)
+
+	for _, line := range strings.Split(data, "\r\n") {
+		if len(line) != 0 && !strings.HasPrefix(line, "#") {
+			result := strings.Split(line, ":")
+			if len(result) == 2 {
+				info[result[0]] = result[1]
+			}
 		}
 	}
 
-	return nil
+	return info
 }
