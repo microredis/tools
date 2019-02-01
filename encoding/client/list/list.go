e095cb8f737de62e19c501c6e0f8874f2d44a0fd
mikefaraponov <mikefaraponov@gmail.com>
2019-02-01 21:39:48 +0200

    Refactored

--- encoding/client/list/list.go
+++ encoding/client/list/list.go
@@ -9,42 +9,33 @@ import (
 
 func Unmarshal(data []byte, v interface{}) error {
 	rv := reflect.ValueOf(v)
-
 	if rv.Kind() != reflect.Ptr {
 		return fmt.Errorf("non-pointer %v", rv.Type())
 	}
-
 	rv = rv.Elem()
-
 	if rv.Kind() != reflect.Slice {
 		return fmt.Errorf("invalid type of v %s", rv.Kind())
 	}
 	clients := strings.Split(string(data), "\r\n")
-
 	rv.Set(reflect.MakeSlice(rv.Type(), len(clients), len(clients)))
 	rv.SetCap(len(clients))
 	rv.SetLen(len(clients))
+	return parseClientList(clients, rv)
+}
 
+func parseClientList(clients []string, rv reflect.Value) error {
 	for i, line := range clients {
 		client := rv.Index(i)
-
 		details := strings.Split(line, " ")
-
 		options := make(map[string]string, len(details))
-
 		for _, item := range details {
 			kv := strings.SplitN(item, "=", 2)
 			options[kv[0]] = kv[1]
 		}
-
 		for i := 0; i < client.NumField(); i++ {
-
 			field := client.Type().Field(i)
-
 			tag := field.Tag.Get("client_list")
-
 			value := options[tag]
-
 			switch field.Type.Kind() {
 			case reflect.Int64:
 				result, err := strconv.ParseInt(value, 10, 64)
@@ -64,6 +55,5 @@ func Unmarshal(data []byte, v interface{}) error {
 			}
 		}
 	}
-
 	return nil
 }
--- encoding/info/info.go
+++ encoding/info/info.go
@@ -9,39 +9,30 @@ import (
 
 func Unmarshal(data []byte, v interface{}) error {
 	rv := reflect.ValueOf(v)
-
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
 		return errors.New("invalid type of v")
 	}
-
 	if !rv.IsValid() {
 		return errors.New("invalid")
 	}
-
 	info := parseInfo(string(data))
-
 	return fillObjectFields(info, rv)
 }
 
 func fillObjectFields(info map[string]string, v reflect.Value) error {
 	for i := 0; i < v.Elem().NumField(); i += 1 {
 		field := v.Elem().Type().Field(i)
-
 		tag := field.Tag.Get("info")
-
 		if tag == "" {
 			continue
 		}
-
 		if result := strings.Split(tag, ","); len(result) == 2 && result[1] == "keyspace" {
 			if err := parseKeySpace(info, v.Elem().Field(i)); err != nil {
 				return err
 			}
 			continue
 		}
-
 		value := info[tag]
-
 		switch field.Type.Kind() {
 		case reflect.Int64:
 			result, err := strconv.ParseInt(value, 10, 64)
@@ -65,7 +56,6 @@ func fillObjectFields(info map[string]string, v reflect.Value) error {
 
 func parseKeySpace(info map[string]string, v reflect.Value) error {
 	keyspace := make(map[string]map[string]int64)
-
 	for key, value := range info {
 		if strings.HasPrefix(key, "db") {
 			keyspace[key] = make(map[string]int64)
@@ -79,14 +69,12 @@ func parseKeySpace(info map[string]string, v reflect.Value) error {
 			}
 		}
 	}
-
 	v.Set(reflect.ValueOf(keyspace))
 	return nil
 }
 
 func parseInfo(data string) map[string]string {
 	info := make(map[string]string)
-
 	for _, line := range strings.Split(data, "\r\n") {
 		if len(line) != 0 && !strings.HasPrefix(line, "#") {
 			result := strings.Split(line, ":")
@@ -95,6 +83,5 @@ func parseInfo(data string) map[string]string {
 			}
 		}
 	}
-
 	return info
 }
