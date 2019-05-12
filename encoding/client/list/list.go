661b1be9ae4d6b93e806cd00e6883441fddb7bf3
mikefaraponov <you@example.com>
2019-05-12 12:36:37 +0300

    Added new way to handle info

--- encoding/client/list/list.go
+++ encoding/client/list/list.go
@@ -1,30 +1,59 @@
 package list
 
 import (
-	"bytes"
-	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
-var newLine = []byte("\r\n")
-
-var space = []byte(" ")
-
 func Unmarshal(data []byte, v interface{}) error {
-	clients := bytes.Split(data, newLine)
-	list := make([]map[string]string, len(clients))
-	for i := range clients {
-		details := bytes.Split(clients[i], space)
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr {
+		return fmt.Errorf("non-pointer %v", rv.Type())
+	}
+	rv = rv.Elem()
+	if rv.Kind() != reflect.Slice {
+		return fmt.Errorf("invalid type of v %s", rv.Kind())
+	}
+	clients := strings.Split(string(data), "\r\n")
+	rv.Set(reflect.MakeSlice(rv.Type(), len(clients), len(clients)))
+	rv.SetCap(len(clients))
+	rv.SetLen(len(clients))
+	return parseClientList(clients, rv)
+}
+
+func parseClientList(clients []string, rv reflect.Value) error {
+	for i, line := range clients {
+		client := rv.Index(i)
+		details := strings.Split(line, " ")
 		options := make(map[string]string, len(details))
-		for i := range details {
-			kv := strings.SplitN(string(details[i]), "=", 2)
+		for _, item := range details {
+			kv := strings.SplitN(item, "=", 2)
 			options[kv[0]] = kv[1]
 		}
-		list[i] = options
+		for i := 0; i < client.NumField(); i++ {
+			field := client.Type().Field(i)
+			tag := field.Tag.Get("client_list")
+			value := options[tag]
+			switch field.Type.Kind() {
+			case reflect.Int64:
+				result, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					return err
+				}
+				client.Field(i).SetInt(result)
+			case reflect.String:
+				client.Field(i).SetString(value)
+			case reflect.Float64:
+				reflect.TypeOf(new(int))
+				result, err := strconv.ParseFloat(value, 64)
+				if err != nil {
+					return err
+				}
+				client.Field(i).SetFloat(result)
+			}
+		}
 	}
-	result, err := json.Marshal(list)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(result, v)
+	return nil
 }
--- encoding/client/list/list_test.go
+++ encoding/client/list/list_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Client struct {
-	Id   int64  `json:"id,string"`
-	Name string `json:"name"`
-	Age  int64  `json:"age,string"`
+	Id   int64  `client_list:"id"`
+	Name string `client_list:"name"`
+	Age  int64  `client_list:"age"`
 }
 
 func TestUnmarshal(t *testing.T) {
